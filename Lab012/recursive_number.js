cde32efb5dbfa2936bb690673155003aed81ce2e
ahmadabuhamdieh <ahmadabuhamdieh@gmail.com>
2022-09-22 01:01:58 -0500

    commit

--- Lab012/recursive_number.js
+++ Lab012/recursive_number.js
@@ -3,13 +3,13 @@ const readline = require("readline").createInterface({
     output: process.stdout,
   });
 
-var recursiveReadLine = function () {
+var getNumber = function () {
     readline.question('enter a number or write stop to stop: ', function (answer) {
       if (answer == 'stop') //we need some base case, for recursion
         return readline.close(); //closing RL and returning from function.
      
-      recursiveReadLine(); //Calling this function again to ask new question
+        getNumber(); //Calling this function again to ask new question
     });
   };
   
-  recursiveReadLine(); //we have to actually start our recursion somehow
+  getNumber(); //we have to actually start our recursion somehow
